f70e0224f87b733bea6f61f277cda95c2348fe24
Josh Harvey <josh@lamassu.is>
2015-02-24 16:31:00 -0500

    WIP

--- tests/unit/queues.js
+++ tests/unit/queues.js
@@ -1,14 +1,23 @@
-/* global define */
+var path = require('path')
+var describe = require('mocha').describe
+var before = require('mocha').before
+var it = require('mocha').it
+var assert = require('assert')
 
-var queues = require('../../../lib/queues')
+var queue = require('../../lib/queue')
 
-define(function (require) {
-  var registerSuite = require('intern!object')
+describe('Queue', function () {
+  before(function () {
+    queue.setRoot(path.resolve(__dirname, '../fixtures/queues'))
+  })
 
-  registerSuite({
-    'passing test': function () {},
-    'failing test': function () {
-      throw new Error('Oops')
-    }
+  it('should load a queue', function () {
+    return queue.load()
+    .then(function () {
+      assert.equal(queue.get('test1', 0).foo, 'bar')
+      assert.equal(queue.get('test1', 1).baz, 'qux')
+      assert.equal(queue.get('test2', 0).take.me, 'out')
+      assert.equal(queue.get('test2', 1).to, 'the ballgame')
+    })
   })
 })
