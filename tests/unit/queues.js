d2472abf6d234160c1235264fb51ea4d07ba80eb
Josh Harvey <josh@lamassu.is>
2015-02-24 19:38:31 -0500

    should not publish to queues owned by others

--- tests/unit/queues.js
+++ tests/unit/queues.js
@@ -4,12 +4,14 @@ var path = require('path')
 var Promise = require('bluebird')
 
 var fs = Promise.promisifyAll(require('fs-extra'))
+var R = require('ramda')
 
 var describe = require('mocha').describe
 var beforeEach = require('mocha').beforeEach
 var it = require('mocha').it
 var assert = require('assert')
 
+var E = require('../../lib/error')
 var queue = require('../../lib/queue')
 
 describe('Queue', function () {
@@ -17,30 +19,33 @@ describe('Queue', function () {
     var fixtures = path.resolve(__dirname, '../fixtures/queues')
     return fs.ensureDirAsync('/tmp/queues')
     .then(function () {
+      return fs.removeAsync('/tmp/queues')
+    })
+    .then(function () {
       return fs.copyAsync(fixtures, '/tmp/queues')
     })
     .then(function () {
-      queue.setRoot('/tmp/queues')
+      queue.config({root: '/tmp/queues', machineId: '1'})
       queue.clear()
       return queue.load()
     })
   })
 
   it('should have loaded the queue', function () {
-    assert.equal(queue.get('test1', 0).foo, 'bar')
-    assert.equal(queue.get('test1', 1).baz, 'qux')
-    assert.equal(queue.get('test2', 0).take.me, 'out')
-    assert.equal(queue.get('test2', 1).to, 'the ballgame')
+    assert.equal(queue.get('1-test1', 0).foo, 'bar')
+    assert.equal(queue.get('1-test1', 1).baz, 'qux')
+    assert.equal(queue.get('2-test2', 0).take.me, 'out')
+    assert.equal(queue.get('2-test2', 1).to, 'the ballgame')
   })
 
   it('should have diff', function () {
     var lastCounters = [
-      {queueName: 'test1', lastCounter: 1},
-      {queueName: 'test2', lastCounter: 0}
+      {queueName: '1-test1', lastCounter: 1},
+      {queueName: '2-test2', lastCounter: 0}
     ]
     var res = queue.diff(lastCounters)
     assert.deepEqual(res, [
-      {queueName: 'test2', diff: [
+      {queueName: '2-test2', diff: [
         {i: 1, rec: {to: 'the ballgame'}}
       ]}
     ])
@@ -48,12 +53,12 @@ describe('Queue', function () {
 
   it('should return correct value on new queue diff', function () {
     var lastCounters = [
-      {queueName: 'test1', lastCounter: 1},
-      {queueName: 'test2', lastCounter: null}
+      {queueName: '1-test1', lastCounter: 1},
+      {queueName: '2-test2', lastCounter: null}
     ]
     var res = queue.diff(lastCounters)
     assert.deepEqual(res, [
-      {queueName: 'test2', diff: [
+      {queueName: '2-test2', diff: [
         {i: 0, rec: {take: {me: 'out'}}},
         {i: 1, rec: {to: 'the ballgame'}}
       ]}
@@ -62,8 +67,8 @@ describe('Queue', function () {
 
   it('should return empty list when up to date', function () {
     var lastCounters = [
-      {queueName: 'test1', lastCounter: 1},
-      {queueName: 'test2', lastCounter: 1}
+      {queueName: '1-test1', lastCounter: 1},
+      {queueName: '2-test2', lastCounter: 1}
     ]
     var res = queue.diff(lastCounters)
     assert.deepEqual(res, [])
@@ -71,8 +76,8 @@ describe('Queue', function () {
 
   it('should successfully publish a record', function () {
     var rec = {buy: 'me', some: 'peanuts'}
-    queue.publish('test2', rec)
-    assert.equal(queue.get('test2', 2).buy, 'me')
+    queue.publish('1-test1', rec)
+    assert.equal(queue.get('1-test1', 2).buy, 'me')
 
     // Wait until file is saved
     return Promise.delay(10)
@@ -81,7 +86,12 @@ describe('Queue', function () {
       return queue.load()
     })
     .then(function () {
-      assert.equal(queue.get('test2', 2).buy, 'me')
+      assert.equal(queue.get('1-test1', 2).buy, 'me')
     })
+  })
+
+  it('should not publish to queues it doesn\'t own', function () {
+    var rec = {buy: 'me', some: 'peanuts'}
+    assert.throws(R.partial(queue.publish, '2-test2', rec), E.ReadOnlyError)
   })
 })
